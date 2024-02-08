eb21a6ca218b247f6132888c14421929c50bcc4d
Digerati <cabal@digerati.design>
2024-02-08 18:08:27 +0000

    Add Webflow Modal

---
+++ src/digerati/modal.ts
@@ -0,0 +1,33 @@
+/**
+ * Dynamically populate Speaker Modal
+ * with CMS Collection Item Properties
+ * from corresponding Speaker Slider.
+ */
+export const modal = () => {
+    const showModalLinks = document.querySelectorAll('[data-modal="show"]'),
+        hideModalLinks = document.querySelectorAll('[data-modal="hide"]'),
+        modal = document.querySelector('[data-modal="modal"]');
+    if (!showModalLinks || !hideModalLinks || !modal) {
+        return;
+    }
+    hideModalLinks.forEach((hideModalLink) => {
+        hideModalLink.addEventListener('click', () => {
+            console.log(hideModalLink);
+            modal.classList.add('hide');
+        });
+    });
+    showModalLinks.forEach((showModalLink) => {
+        showModalLink.addEventListener('click', () => {
+            console.log(showModalLink);
+            let speaker = showModalLink.closest('[data-speaker="slide"]');
+            if (!speaker) {
+                return false;
+            }
+            modal.querySelector(['[data-modal="name"]']).innerText = speaker.querySelector('[data-speaker="name"]').innerText;
+            modal.querySelector(['[data-modal="title"]']).innerText = speaker.querySelector('[data-speaker="title"').innerText;
+            modal.querySelector(['[data-modal="photo"]']).src = speaker.querySelector('[data-speaker="photo"]').src;
+            modal.querySelector(['[data-modal="biography"]']).innerHTML = speaker.querySelector('[data-speaker="biography"').innerHTML;
+            modal.classList.remove('hide');
+        }, { passive: true });
+    });
+};
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import { greetUser } from '$utils/greet';
+import { modal } from '$digerati/modal';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+  modal();
 });
